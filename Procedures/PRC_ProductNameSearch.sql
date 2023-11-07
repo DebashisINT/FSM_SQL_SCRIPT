464867f50dec67cac5aa9184d33baf6c43ac2fff
SanchitaINT <82153205+SanchitaINT@users.noreply.github.com>
2023-11-07 11:28:24 +0530

    COMMIT

--- Procedures/PRC_ProductNameSearch.sql
+++ Procedures/PRC_ProductNameSearch.sql
@@ -7,13 +7,34 @@ GO
 ALTER PROCEDURE [dbo].[PRC_ProductNameSearch]  
 (
 @USER_ID BIGINT=0,
-@SearchKey varchaR(50) =''
+@SearchKey varchaR(50) ='',
+-- Rev 2.0
+@Action nvarchar(max)=''
+-- End of Rev 2.0
 )
 AS
 /*******************************************************************************************************************************************************************************************
 1.0		v2.0.30		Pratik  	24-05-2022		On demand product search for "Stock Position Report"
+2.0		V2.0.43		Sanchita	06-11-2023		On demand search is required in Product Master & Projection Entry. Mantis: 26858
 ********************************************************************************************************************************************************************************************/
 BEGIN
-		select top(10)sProducts_ID,sProducts_Code,sProducts_Name,sProducts_Description from Master_sProducts
-		where (sProducts_Name like '%' + @SearchKey + '%') or  (sProducts_Code like '%' + @SearchKey + '%')
+		-- Rev 2.0
+		IF(@action='SearchByColor')
+		BEGIN
+			 select top 10 Color_ID, Color_Name from Master_Color where Color_Name LIKE '%'+@SearchKey+'%' 
+		END
+		ELSE IF(@action='SearchByBrand')
+		BEGIN
+			 select top 10 Brand_Id ,Brand_Name from tbl_master_brand where Brand_IsActive=1 AND Brand_Name LIKE '%'+@SearchKey+'%'
+		END
+		ELSE
+		BEGIN
+		-- End of Rev 2.0
+			select top(10)sProducts_ID,sProducts_Code,sProducts_Name,sProducts_Description from Master_sProducts
+			where (sProducts_Name like '%' + @SearchKey + '%') or  (sProducts_Code like '%' + @SearchKey + '%')
+	   -- Rev 2.0
+	   END
+	   -- End of Rev 2.0
+	
 END
+GO
